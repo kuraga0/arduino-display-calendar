772295601d6b3284380c182120cc8dc47725f9aa
Andrew <gus123tractor@gmail.com>
2026-07-09 15:39:53 +0400

    print calendar

--- src/main.cpp
+++ src/main.cpp
@@ -29,29 +29,46 @@ MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 #define CURRENT_SECONDS 00
 #define CURRENT_MINUTES 30
 #define CURRENT_HOURS 10
-#define CURRENT_DAY_OF_WEEK 3
-#define CURRENT_DAY_OF_MONTH 3
-#define CURRENT_MONTH 03
+#define CURRENT_DAY_OF_WEEK 5
+#define CURRENT_DAY_OF_MONTH 9
+#define CURRENT_MONTH 07
 #define CURRENT_YEAR 2026
 
 virtuabotixRTC RTC(DS1302_CLK_PIN, DS1302_DAT_PIN, DS1302_RST_PIN);
 
 // == END CONFIGURATION ==========
 
+// get number of empty cells at month start
+int getLeadingOffset(int year, int month) {
+	// Zellers congruence
+	if (month < 3) {
+		month += 12;
+		year -= 1;
+	}
+	int k = year % 100;
+	int j = year / 100;
+	int h = (1 + 13 * (month + 1) / 5 + k + k / 4 + j / 4 + 5 * j) % 7;
+	// h: 0=Sat, 1=Sun, 2=Mon, 3=Tue, 4=Wed, 5=Thu, 6=Fri
+
+	int weekdayMon0 = (h + 5) % 7; // into 0=Mon..6=Sun
+
+	return weekdayMon0;
+}
+
 struct Vec2 {
   double x;
   double y;
 };
 
-Vec2 day_to_coord(int day) {
+Vec2 day_to_coord(int year, int month, int day) {
   int y = day / 7;
-  int x = day - (7 * y);
+  int x = (getLeadingOffset(year, month)) + ( day - (7 * y) );
 
   return Vec2{x - 1, y};
 }
 
-void print_date(int day) {
-  Vec2 pos = day_to_coord(day);
+void print_date(int year, int month, int day) {
+  Vec2 pos = day_to_coord(year, month, day);
 
   myDisplay.getGraphicObject()->setPoint(pos.x, pos.y, true);
 }
@@ -81,11 +98,11 @@ void loop() {
 	RTC.updateTime();
 	// print_date(RTC.dayofmonth);
 
-	print_date(RTC.seconds);
+	print_date(RTC.year, RTC.month, RTC.dayofmonth);
 
 	Serial.print("Current time: ");
 	Serial.print(RTC.seconds);
 	Serial.println(); 
 
-	delay(1000); 
+	delay(60*1*1000); 
 }
